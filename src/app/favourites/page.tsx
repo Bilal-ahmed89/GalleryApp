8535ac44c4091c818711806ac2f846dceb40f175
bilal-ahmed89 <ba1770490@gmail.com>
2023-08-31 14:16:14 +0500

    added in the favourites

--- src/app/favourites/page.tsx
+++ src/app/favourites/page.tsx
@@ -1,38 +1,33 @@
 
-import UploadButton from './upload-button';
+import { ForceRefresh } from '@/components/ForceRefresh';
 import cloudinary from 'cloudinary'
 import { CldImage } from 'next-cloudinary';
-import CloudinaryImage from './cloudinary-image';
-
-export type SearchResult = {
-    public_id: string
-    tags: string[]
-};
+import CloudinaryImage from '../gallery/cloudinary-image';
+import { SearchResult } from '../gallery/page';
 
 
-async function GalleryPage() {
+async function FavouritesPage() {
 
     const results = await cloudinary.v2.search
-        .expression('resource_type:image')
+        .expression('resource_type:image AND tags=favourites')
         .sort_by('created_at', 'desc')
         .with_field("tags")
-        .max_results(20)
+        .max_results(30)
         .execute() as { resources: SearchResult[] }
 
 
     return (
         <section>
-
+            <ForceRefresh />
             <div className='flex flex-col gap-8'>
                 <div className='flex justify-between'>
-                    <h1 className='text-4xl font-bold'>Gallery</h1>
-                    <UploadButton />
+                    <h1 className='text-4xl font-bold'>Favourite Images</h1>
                 </div>
                 <div className='grid grid-cols-4 gap-4'>
                     {
                         results.resources.map((result) => (
                             <CloudinaryImage
-                                path='/gallery'
+                                path='/favourites'
                                 key={result.public_id}
                                 imagedata={result}
                                 alt="an image of someting"
@@ -47,4 +42,4 @@ async function GalleryPage() {
     )
 }
 
-export default GalleryPage;
+export default FavouritesPage;
--- src/app/gallery/cloudinary-image.tsx
+++ src/app/gallery/cloudinary-image.tsx
@@ -1,22 +1,43 @@
 "use client"
-import { CldImage } from 'next-cloudinary';
+import { CldImage, CldImageProps, CldOgImageProps } from 'next-cloudinary';
 import { Heart } from './../../components/ui/icons/heart';
-import {MarkAsFavouriteAction} from './actions';
-import {useTransition} from 'react'
+import { MarkAsFavouriteAction } from './actions';
+import { useTransition } from 'react'
+import { SearchResult } from './page';
+import { FullHeart } from '@/components/ui/icons/FullHeart';
+import { useState } from 'react';
 
 
-function CloudinaryImage(props: any & {publicid : string}) {
-    const [transition , startTransition] = useTransition()
+function CloudinaryImage(props: {imagedata : SearchResult; path : string} & Omit<CldImageProps , "src">) {
+
+    const [transition, startTransition] = useTransition()
+     const {imagedata } = props
+
+    const [isFavourited, setIsFavourited] =  useState(imagedata.tags.includes('favourites')) 
+
+
     return (
         <div className='relative' >
-            <CldImage {...props} />
-            <Heart className="absolute top-2 right-2 hover:text-red-500 cursor-pointer" 
-            onClick={()=>{
-                startTransition(()=>{
-                    MarkAsFavouriteAction(props.publicid)
-                })
-            }}
+            <CldImage {...props} src={imagedata.public_id} />
+            {
+                isFavourited  ? 
+                <FullHeart className="absolute top-2 right-2 hover:text-white text-red-500 cursor-pointer"
+                onClick={() => {
+                    startTransition(() => {
+                        MarkAsFavouriteAction(imagedata.public_id, false, props.path)
+                    })
+                }}
             />
+                :
+
+                    <Heart className="absolute top-2 right-2 hover:text-red-500 cursor-pointer"
+                        onClick={() => {
+                            startTransition(() => {
+                                MarkAsFavouriteAction(imagedata.public_id, true,props.path)
+                            })
+                        }}
+                    />
+            }
         </div>
     )
 
